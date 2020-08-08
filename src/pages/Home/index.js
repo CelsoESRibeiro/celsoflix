493172e82c713771a3ae5c2afad306f3c9a0ca8b
Celso Ribeiro <celsoed2011@gmail.com>
2020-08-07 23:47:24 -0300

    commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,7 @@ function Home() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription={" Esté é uma curadoria de estudos e ensaios sobre o mundo da 7º Arte, Explorando tudos os aspectos que culminam na magia do Cinema "}
       />
 
       <Carousel
