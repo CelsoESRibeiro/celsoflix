a2219c4acc079ae92936d5a08a5bfb03e2c685da
Celso Ribeiro <celsoed2011@gmail.com>
2020-08-09 23:06:46 -0300

    commit

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled,  { css } from 'styled-components';
-
+import styled, { css } from 'styled-components';
 
 const FormFieldWrapper = styled.div`
-  position: relative;
-
+  position: relative; 
   textarea {
     min-height: 150px;
   }
   input[type="color"] {
     padding-left: 56px;
   }
-`; 
+`;
 
 const Label = styled.label``;
 Label.Text = styled.span`
@@ -31,8 +29,7 @@ Label.Text = styled.span`
   font-weight: 300;
   
   transition: .1s ease-in-out;
-`; 
- 
+`;
 
 const Input = styled.input`
   background: #53585D;
@@ -57,38 +54,31 @@ const Input = styled.input`
   &:focus {
     border-bottom-color: var(--primary);
   }
-
-  &:focus:not([type='color']) + ${Label.Text} {
+  &:focus:not([type="color"]) + span {
     transform: scale(.6) translateY(-10px);
-  } 
-
-  ${({ value }) => {
-    const hasValue = value.length > 0;
-    return hasValue && css`
-        &:not([type='color']) + ${Label.Text} {
-          transform: scale(.6) translateY(-10px);
-        }
-      `;
   }
-  
-} 
+  ${({ hasValue }) => hasValue && css`
+    &:not([type="color"]) + span {
+      transform: scale(.6) translateY(-10px);
+    }
+  `}
 `;
 
 function FormField({
-  label, type, name, value, onChange,
+  label, type, name, value, onChange, suggestions,
 }) {
   const fieldId = `id_${name}`;
   const isTypeTextarea = type === 'textarea';
-  const  tag = isTypeTextarea ? 'textarea' : 'input';
+  const tag = isTypeTextarea ? 'textarea' : 'input';
 
-  const hasValue = value.length;
+  const hasValue = Boolean(value.length);
+  const hasSuggestions = Boolean(suggestions.length);
 
   return (
     <FormFieldWrapper>
       <Label
         htmlFor={fieldId}
       >
-        
         <Input
           as={tag}
           id={fieldId}
@@ -97,11 +87,27 @@ function FormField({
           name={name}
           hasValue={hasValue}
           onChange={onChange}
+          autoComplete={hasSuggestions ? 'off' : 'on'}
+          list={hasSuggestions ? `suggestionFor_${fieldId}` : undefined}
         />
         <Label.Text>
-        {label}
-        : 
+          {label}
+          :
         </Label.Text>
+        {
+          hasSuggestions && (
+            <datalist id={`suggestionFor_${fieldId}`}>
+              {
+              suggestions.map((suggestion) => (
+                <option value={suggestion} key={`suggestionFor_${fieldId}_option${suggestion}`}>
+                  {suggestion}
+                </option>
+              ))
+            }
+            </datalist>
+          )
+        }
+
       </Label>
     </FormFieldWrapper>
   );
@@ -111,14 +117,16 @@ FormField.defaultProps = {
   type: 'text',
   value: '',
   onChange: () => {},
+  suggestions: [],
 };
 
-FormField.propType = {
-  Label: PropTypes.string.isRequired,
-  type: PropTypes.string, 
-  name: PropTypes.string.isRequired, 
-  value: PropTypes.string, 
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
   onChange: PropTypes.func,
+  suggestions: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default FormField;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -3,50 +3,55 @@ import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
+import useForm from '../../../hooks/useForm';
 
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
-    discricao: '',
+    descricao: '',
     cor: '',
   };
 
+  const { handleChange, values, clearForm } = useForm(valoresIniciais);
+
   const [categorias, setCategorias] = useState([]);
-  const [values, setValues] = useState(valoresIniciais);
-
-  function setValue(chave, valor) {
-    setValues({
-      ...values,
-      [chave]: valor,
-    });
-  }
-
-  function handleChange(infosDoEvento) {
-    setValue(
-      infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value,
-    );
-  }
-
 
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
       ? 'http://localhost:8080/categorias'
-      : 'https://celsoflix.herokuapp.com/categorias';
+      : 'https://devsoutinhoflix.herokuapp.com/categorias';
+    // E a ju ama variáveis
     fetch(URL_TOP)
-      .then(async (respostaDoServidor) =>{
-          const resposta = await respostaDoServidor.json();
-          setCategorias([
-             ...resposta,
-          ]);
+      .then(async (respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
       });
 
+    // setTimeout(() => {
+    //   setCategorias([
+    //     ...categorias,
+    //     {
+    //       id: 1,
+    //       nome: 'Front End',
+    //       descricao: 'Uma categoria bacanudassa',
+    //       cor: '#cbd1ff',
+    //     },
+    //     {
+    //       id: 2,
+    //       nome: 'Back End',
+    //       descricao: 'Outra categoria bacanudassa',
+    //       cor: '#cbd1ff',
+    //     },
+    //   ]);
+    // }, 4 * 1000);
   }, []);
 
   return (
     <PageDefault>
       <h1>
-        Cadastro de Categoria
+        Cadastro de Categoria:
         {values.nome}
       </h1>
 
@@ -57,7 +62,7 @@ function CadastroCategoria() {
           values,
         ]);
 
-        setValues({ valoresIniciais });
+        clearForm();
       }}
       >
 
@@ -91,15 +96,15 @@ function CadastroCategoria() {
 
       {categorias.length === 0 && (
         <div>
-           {/*Carregando...*/}
-           Loading...
+          {/* Cargando... */}
+          Loading...
         </div>
       )}
 
       <ul>
         {categorias.map((categoria) => (
-          <li key={`${categoria.nome}`}>
-            {categoria.nome}
+          <li key={`${categoria.titulo}`}>
+            {categoria.titulo}
           </li>
         ))}
       </ul>
